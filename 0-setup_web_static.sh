630d05fbb439d189333b1179626d32a03662559d
Crib1212 <igwepatrickchinedu@gmail.com>
2024-02-04 18:59:46 +0100

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,36 +1,38 @@
 #!/usr/bin/env bash
-# prepares simple nginx servers for static deployment of `web-static`
-service nginx status
-if (( $? != 0 )); then
-    apt-get -y update
-    apt-get -y install nginx
-    find /var/www/html/index.html
-    if (( $? != 0 )); then
-        mkdir -p /var/www/html/
-        echo 'Holberton School' > /var/www/html/index.html
-    fi
-    service nginx restart
-fi
+# Sets up a web server for deployment of web_static.
 
+apt-get update
+apt-get install -y nginx
+
+mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-find /data/web_static/releases/test/index.html
-if (( $? != 0 )); then
-    mkdir -p /data/web_static/releases/test/
-    echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-    ln -sf /data/web_static/releases/test/ /data/web_static/current
-fi
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu:ubuntu /data/
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-grep -q "location \/hbnb_static\/ {$" /etc/nginx/sites-available/default
-if (( $? != 0 )); then
-    cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bup
-    sed -i "0,/^\tlocation \/ {$/s/^\tlocation \/ {$/\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n\t}\n\n\tlocation \/ {/" /etc/nginx/sites-available/default
-    service nginx reload
-fi
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+	alias /data/web_static/current;
+	index index.html index.htm;
+    }
+
+    location /redirect_me {
+	return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
